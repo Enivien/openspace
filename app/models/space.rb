0b183cb11a7167dc0fe08de67d4091e2ae26b1a4
Irvin Hoogland <irvinhoogland@gmail.com>
2018-05-23 17:24:24 +0800

    Update models

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,4 +1,6 @@
 class Space < ApplicationRecord
   belongs_to :amenity
   belongs_to :user
+  has_many :reviews
+  has_many :favorites
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :spaces
+  has_many :favorites
+  has_many :bookings
+  has_many :messages
+  has_many :conversations through: :messages
+  has_many :spaces through: :favorites
 end
+
