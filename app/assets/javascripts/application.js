890e2a04f1f0e00878a2b0beee0a038686fa8d56
Philipp Preuss <philipp.arian@gmail.com>
2018-05-23 17:56:51 +0800

    clean up initial settings

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,4 @@
 //= require rails-ujs
-//= require tabler
-//= require tabler.plugins
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
+//= require jquery
 //= require_tree .
+
