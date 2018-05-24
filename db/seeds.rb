da4e5523db8fe269c0a9dd0dd809fe3cf4cb273c
Irvin Hoogland <irvinhoogland@gmail.com>
2018-05-24 12:49:31 +0800

    Add seed_destroy

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
+
 5.times do
   User.create(
     first_name: Faker::Name.first_name,
