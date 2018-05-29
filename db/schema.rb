e1a39e420156788926d325bf94488a12c56e5628
Ryan Clogg <ryan.clogg@gmail.com>
2018-05-29 16:04:13 +0800

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -68,10 +68,9 @@ ActiveRecord::Schema.define(version: 2018_05_29_074138) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "guest"
+    t.string "booked_activity"
     t.integer "amount_cents", default: 0, null: false
     t.string "status", default: "Pending"
-    t.string "booked_activity"
-
     t.index ["space_id"], name: "index_bookings_on_space_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
