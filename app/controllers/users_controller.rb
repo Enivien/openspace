c61116fd7d0ea71b3ee693947844de0e6531617f
Ryan Clogg <ryan.clogg@gmail.com>
2018-05-30 15:37:02 +0800

    Doesnt work dont know why thhis is happenig

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :avatar, :avatar_cache, :bio)
+    params.require(:user).permit(:first_name, :last_name, :avatar, :avatar_cache, :bio, :email)
   end
 
   def set_user
