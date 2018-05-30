2165456a560093b8844ab21d28a746aafd0125c6
Irvin Hoogland <irvinhoogland@gmail.com>
2018-05-30 09:41:44 +0800

    Heroku debug minus dropzone

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,8 @@
 //= require rails-ujs
 //= require jquery3
 //= require popper
+//= require tabler
+//= require tabler.plugins
 
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
